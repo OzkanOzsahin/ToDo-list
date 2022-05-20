ec760e3710d6c344c00b81c7839b781641d33bfb
OzkanOzsahin <92115875+OzkanOzsahin@users.noreply.github.com>
2022-05-20 17:05:11 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-
-
 const url = 'http://localhost:3000';
-
-
+const taskList = document.getElementById("list");
 
 // POST request
 
 const postData = async (value) => {
- 
 const data = {description: value, done: false};
 
   try {
@@ -43,68 +39,40 @@ getData()
 
 async function createDom() {
   
-  const data = await getData();
-  console.log(data);
+  const data = await getData(); 
+   console.log(data)
   data.forEach(value => {
     const listItem = document.createElement("li"); // Create li element.
     const task = document.createElement("p"); // Create p element.
     const text = document.createTextNode(value.description); // Create text for list item.
-  });
-    // comment: Get value._id put in paragraphs element.
-    // comment: <p id="value._id"> eten </p>
-    //const para = document.createElement("p");
-   // const node = document.createTextNode("This is a paragraph.");
-    //para.appendChild(text); // Append text to p element.
-   // document.getElementById("myDIV").appendChild(para);
-    //listItem.appendChild(task); // Append p to list item.
-    // Create a delete button
-  var deleteButton = document.createElement("button");
-  var deleteButton = document.createElement("delete");
-  deleteButton.setAttribute("class", "far fa-trash-alt");
-
-  
-  //Create button section
-  var buttons = document.createElement("div"); // 
+  listItem.innerHTML = value.description;
+  const buttons = document.createElement("div");  // 
   buttons.setAttribute("class", "buttons");
- 
+  const deleteButton = document.createElement("button"); 
+  deleteButton.setAttribute("class", "far fa-trash-alt");
   buttons.appendChild(deleteButton);    //deleteButton
   listItem.appendChild(buttons);
+   taskList.appendChild(listItem);
 
-
-  // comment: stuur id naar functie delete data
-
-  fetch('http://localhost:3000', {
-    method: "DELETE",
-    headers: {
-        'Content-type': 'application/json'
-    }
-})
-.then(res => {
-    if (res.ok) { console.log("HTTP request successful") }
-    else { console.log("HTTP request unsuccessful") }
-    return res
-})
-.then(res => res.json())
-.then(data => console.log(data))
-.catch(error => console.log(error))
-
-    deleteButton.addEventListener('click', function () {
-    console.log(getData);
-    console.log(this.parentElement.parentElement);
-    var parent = this.parentElement.parentElement;
-    parent.parentElement.removeChild(parent);
   });
-  //Add new list item to list
-  var list = document.getElementById("list");
-  list.appendChild(listItem);
-    console.log(value) 
-};
+  
+  //Create button section
+  
 
+   
+  
+}
 
-createDom()
+  // Add new list item to list
+    // const list = document.getElementById('list');
+    // list.appendChild(listItem);
+    // console.log(value)
+  
+  createDom()
+
 
 // Creating a new task
-var button = document.getElementById("submit");
+const button = document.getElementById("submit");
 button.addEventListener("click", () => {
   let taskInput = document.getElementById("task").value;
   // Creating the text for list item.
@@ -113,16 +81,18 @@ button.addEventListener("click", () => {
     alert("You have to type a task!");
   } else {
     
-    
     postData(taskInput);
-    createDom();
+    createDom();  
+    
   }
 
-
-  
 });
 
 
+  
+
+
+
 
 
  
@@ -168,4 +138,4 @@ button.addEventListener("click", () => {
 
 
 
-
+  
